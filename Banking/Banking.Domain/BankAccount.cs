0168898cdc7040d0d4758a5b1c9cf64ff3879faf
Jeffry Gonzalez <jeff@hypertheory.com>
2022-09-14 14:23:27 -0400

    done with inheritance

--- Banking/Banking.Domain/BankAccount.cs
+++ Banking/Banking.Domain/BankAccount.cs
@@ -4,7 +4,7 @@ public class BankAccount
 {
 
     private decimal _balance = 5000M; //JFHCI
-    public void Deposit(decimal amountToDeposit)
+    public virtual void Deposit(decimal amountToDeposit)
     {
         _balance += amountToDeposit;
     }
---
+++ Banking/Banking.Domain/GoldAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Domain;
+public class GoldAccount : BankAccount
+{
+    public override void Deposit(decimal amountToDeposit)
+    {
+      
+        base.Deposit(amountToDeposit * 1.10M);
+    }
+
+  
+
+    public void SendToaster()
+    {
+
+    }
+}
+
+public class PlatinumAccount : BankAccount
+{
+    public override void Deposit(decimal amountToDeposit)
+    {
+        base.Deposit(amountToDeposit * 1.25M);
+    }
+}
