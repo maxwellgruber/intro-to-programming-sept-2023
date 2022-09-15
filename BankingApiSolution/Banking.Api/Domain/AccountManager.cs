b1cfb1f01141742e7e367171946fff8ab7d63e58
Jeffry Gonzalez <jeff@hypertheory.com>
2022-09-15 16:58:56 -0400

    done with thursday!

--- BankingApiSolution/Banking.Api/Domain/AccountManager.cs
+++ BankingApiSolution/Banking.Api/Domain/AccountManager.cs
@@ -1,36 +1,56 @@
-﻿using Banking.Api.Models;
+﻿using Banking.Api.Adapters;
+using Banking.Api.Models;
+using MongoDB.Driver;
 
 namespace Banking.Api.Domain;
 
 public class AccountManager
 {
+    private readonly MongoAccountsAdapter _adapter;
+
+    public AccountManager(MongoAccountsAdapter adapter)
+    {
+        _adapter = adapter;
+    }
+
     public async Task<CollectionResponse<AccountSummaryResponse>> GetAllAccountsAsync()
     {
+
+        var accountsProjection = Builders<AccountEntity>.Projection.Expression(a => new AccountSummaryResponse { Id = a.Id, Name = a.Name });
+
+        var data = await _adapter.Accounts.Find(_ => true).Project(accountsProjection).ToListAsync();
         var response = new CollectionResponse<AccountSummaryResponse>
         {
-            Data = new List<AccountSummaryResponse>
-            {
-
-               new AccountSummaryResponse { Id = "1", Name = "Bob Smith"},
-               new AccountSummaryResponse { Id = "2", Name = "Jill Jones"}
-            }
+            Data = data
         };
         return response;
     }
 
     public async Task<AccountSummaryResponse?> GetAccountByIdAsync(string id)
     {
-        var response = new AccountSummaryResponse { Id = id, Name = "Bob Smith" };
+
+        var accountsProjection = Builders<AccountEntity>.Projection.Expression(a => new AccountSummaryResponse { Id = a.Id, Name = a.Name });
+        var filter = Builders<AccountEntity>.Filter.Where(a => a.Id == id);
+        var response = await _adapter.Accounts.Find(filter).Project(accountsProjection).SingleOrDefaultAsync();
         return response;
     }
 
     public async Task<AccountSummaryResponse> CreateAccountAsync(AccountCreateRequest request)
     {
-        var response = new AccountSummaryResponse
+        var accountEntity = new AccountEntity
         {
             Id = Guid.NewGuid().ToString(),
             Name = request.Name
         };
+
+        await _adapter.Accounts.InsertOneAsync(accountEntity);
+
+        var response = new AccountSummaryResponse
+        {
+            Id = accountEntity.Id,
+            Name = accountEntity.Name
+        };
         return response;
+
     }
 }
--- BankingApiSolution/Banking.Api/Program.cs
+++ BankingApiSolution/Banking.Api/Program.cs
@@ -1,3 +1,4 @@
+using Banking.Api.Adapters;
 using Banking.Api.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<AccountManager>();
+
+builder.Services.AddSingleton<MongoAccountsAdapter>(sp =>
+{
+    var connectionString = "mongodb://root:TokyoJoe138!@localhost:27017";
+    return new MongoAccountsAdapter(connectionString);
+});
 
 var app = builder.Build();
 
